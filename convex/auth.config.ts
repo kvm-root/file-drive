806503b8703a514c1af2173f3bcff95987eadd85
kvm-root <161141097+kvm-root@users.noreply.github.com>
2024-02-29 21:07:17 +0100

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://poetic-salmon-66.clerk.accounts.dev",
+      domain: "https://crucial-pika-58.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
